8cb5a1d8f12404df8941b81e3641eb828d2bdd7c
Victor Bravim <victorrafaelbravim@gmail.com>
2024-06-21 14:18:08 -0300

    Add

--- src/components/ProtectedRoute.js
+++ src/components/ProtectedRoute.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Navigate } from 'react-router-dom';
-import { useAuth } from '../AuthContext'; // Assumindo que você está exportando useAuth de AuthContext
+import { useAuth } from '../AuthContext';
 
 const ProtectedRoute = ({ children }) => {
   const { currentUser } = useAuth();
