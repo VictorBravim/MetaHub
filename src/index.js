b9c50081d44f7b7450251bd5876e8cdb40f1be45
Victor Bravim <victorrafaelbravim@gmail.com>
2024-06-26 11:56:22 -0300

    Add

--- src/index.js
+++ src/index.js
@@ -1,7 +1,10 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
+import Modal from 'react-modal';
 import App from './App';
 import './tailwind.css';
+
+Modal.setAppElement('#root');
 
 const container = document.getElementById('root');
 const root = createRoot(container);
