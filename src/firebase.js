6eed82668b747bb0de555b6550bc8f763eb1d435
Victor Bravim <victorrafaelbravim@gmail.com>
2024-06-28 20:58:27 -0300

    Add

--- src/firebase.js
+++ src/firebase.js
@@ -2,13 +2,15 @@ import { initializeApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
 
+//Add your Firebase ApiKey
+
 const firebaseConfig = {
-    apiKey: "AIzaSyDp8TBxIwkI75LBl0LAPBeNZVBfG9D3dKA",
-    authDomain: "metahub-d5df0.firebaseapp.com",
-    projectId: "metahub-d5df0",
-    storageBucket: "metahub-d5df0.appspot.com",
-    messagingSenderId: "863235368484",
-    appId: "1:863235368484:web:3885ff615b1e3f2b519ab1",
+    apiKey: "",
+    authDomain: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: "",
 };
 
 const app = initializeApp(firebaseConfig);
